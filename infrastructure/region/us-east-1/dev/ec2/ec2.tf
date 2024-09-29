d713720e7451be2618c65070874550d81a1defc6
Kay <odetundeoluwakayode@gmail.com>
2024-09-29 17:39:11 +0100

    AWS infrastructure

---
+++ infrastructure/region/us-east-1/dev/ec2/ec2.tf
@@ -0,0 +1,11 @@
+module "ec2_instance" {
+
+  source            = "../../../../..//modules/ec2"
+  aws_ami           = "ami-0440d3b780d96b29d"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "rosi_ec2_instance_1"
+  }
+  region = "us-east-1"
+}
